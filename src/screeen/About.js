bc5e816a04fd63629820ebf3ee20960a13e46eac
Mohd-Fuzail <mohdfuzail489@gmail.com>
2023-10-30 18:05:53 +0530

    adding remaing components

--- src/screeen/About.js
+++ src/screeen/About.js
@@ -1,14 +1,131 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Linking,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
+import React from "react";
 
 const About = () => {
   return (
-    <View>
-      <Text>About</Text>
+    <View style={styles.aboutContainer}>
+      <Text style={styles.mainHeader}> Mohd Fuzail Shaikh </Text>
+      <Text style={styles.paraStyle}> I am a full stack developer 😀 </Text>
+
+      <View>
+        <Image
+          style={styles.imgStyle}
+          source={require('../../assets/m1.png')}
+        />
+      </View>
+
+      <View style={styles.aboutLayout}>
+        <Text style={styles.aboutSubHeader}> About me </Text>
+        <Text style={[styles.paraStyle, styles.aboutPara]}>
+        I am Mohd Fuzail, and I am actively working on enhancing my development skills.
+         My focus areas include JavaScript, React, and React Native. I have successfully
+          completed projects related to these technologies, showcasing my practical experience in application 
+        development and my commitment to continuous improvement in the field of web and mobile development.
+        </Text>
+      </View>
+
+      <Text style={styles.mainHeader}> Follow me on Social Network </Text>
+
+      <View style={styles.menuContainer}>
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() =>
+            Linking.openURL("https://www.instagram.com/mohd.__fuzail")
+          }>
+          <Image
+            style={styles.iconStyle}
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/512/2111/2111463.png",
+            }}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() =>
+            Linking.openURL(
+              "https://www.linkedin.com/in/mohd-fuzail-7bb21a22a"
+            )
+          }>
+          <Image
+            style={styles.iconStyle}
+            source={require('../../assets/linkedin.png')}
+          />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          onPress={() => Linking.openURL("https://github.com/Mohd-Fuzail")}>
+          <Image
+            style={styles.iconStyle}
+            source={require('../../assets/github.png')}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  aboutContainer: {
+    display: "flex",
+    alignItems: "center",
+  },
+
+  imgStyle: {
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+  },
+  mainHeader: {
+    fontSize: 18,
+    color: "#344055",
+    textTransform: "uppercase",
+    fontWeight: "500",
+    marginTop: 30,
+    marginBottom: 10,
+    
+  },
+  paraStyle: {
+    fontSize: 14,
+    color: "#7d7d7d",
+    paddingBottom: 30,
+  },
+  aboutLayout: {
+    backgroundColor: "#4c5dab",
+    paddingHorizontal: 30,
+    marginVertical: 30,
+  },
+  aboutSubHeader: {
+    fontSize: 18,
+    color: "#fff",
+    textTransform: "uppercase",
+    fontWeight: "500",
+    marginVertical: 15,
+    
+    alignSelf: "center",
+  },
+  aboutPara: {
+    color: "#fff",
+  },
+  menuContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+
+  iconStyle: {
+    width: "100%",
+    height: 50,
+    aspectRatio: 1,
+  },
+});
 
 export default About;
-
-const styles = StyleSheet.create({})
--- src/screeen/Contact.js
+++ src/screeen/Contact.js
@@ -1,14 +1,177 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
+import React, { useState } from "react";
+import Checkbox from "expo-checkbox";
 
-const Contact = () => {
+const Contact = ({ navigation }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [message, setMessage] = useState("");
+  const [agree, setAgree] = useState(false);
+
+  const submit = () => {
+    if (!name && !email && !phone && !message) {
+      Alert.alert("Plzz fill all the fields");
+    } else {
+      Alert.alert(`Thank You ${name}`);
+      navigation.navigate("Home");
+    }
+  };
+
   return (
-    <View>
-      <Text>Contact</Text>
+    <View style={styles.mainContainer}>
+      <Text style={styles.mainHeader}> Level up your knowledge </Text>
+
+      <Text style={styles.description}>
+        You can reach us anytime via fuzail@mohd.com
+      </Text>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.labels}> Enter your name </Text>
+        <TextInput
+          style={styles.inputStyle}
+          placeholder={"Mohd Fuzail"}
+          value={name}
+          onChangeText={(userdata) => setName(userdata)}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.labels}> Enter your Email </Text>
+        <TextInput
+          style={styles.inputStyle}
+          placeholder={"demo@fuzail.com"}
+          value={email}
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.labels}> Enter your mobile </Text>
+        <TextInput
+          style={styles.inputStyle}
+          placeholder={"5655252558"}
+          value={phone}
+          onChangeText={(phone) => setPhone(phone)}
+        />
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.labels}> How can we help you? </Text>
+        <TextInput
+          style={[styles.inputStyle, styles.multilineStyle]}
+          placeholder={"Tell us about your self"}
+          value={message}
+          onChangeText={(msg) => setMessage(msg)}
+          numberOfLines={5}
+          multiline={true}
+        />
+      </View>
+
+      {/* checkbox  */}
+
+      <View style={styles.wrapper}>
+        <Checkbox
+          value={agree}
+          onValueChange={() => setAgree(!agree)}
+          color={agree ? "#4630EB" : undefined}
+        />
+        <Text style={styles.wrapperText}>
+          I have read and agreed with the TC
+        </Text>
+      </View>
+
+      {/* submit button  */}
+
+      <TouchableOpacity
+        style={[
+          styles.buttonStyle,
+          {
+            backgroundColor: agree ? "#4630EB" : "grey",
+          },
+        ]}
+        disabled={!agree}
+        onPress={submit}>
+        <Text style={styles.buttonText}> Contact Us </Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    height: "100%",
+    paddingHorizontal: 30,
+    backgroundColor: "#fff",
+  },
+  mainHeader: {
+    fontSize: 20,
+    color: "#344055",
+    fontWeight: "500",
+    paddingTop: 20,
+    paddingBottom: 15,
+  
+    textTransform: "capitalize",
+  },
+  description: {
+    fontSize: 20,
+    color: "#7d7d7d",
+    paddingBottom: 20,
+    
+    lineHeight: 25,
+  },
+
+  inputContainer: {
+    marginTop: 20,
+  },
+  labels: {
+    fontWeight: "bold",
+ 
+    color: "#7d7d7d",
+    paddingBottom: 5,
+    
+    lineHeight: 25,
+  },
+  inputStyle: {
+    borderWidth: 1,
+    borderColor: "rgba(0, 0, 0, 0.3)",
+    paddingHorizontal: 15,
+    paddingVertical: 6,
+    borderRadius: 2,
+  },
+  multiineStyle: {
+    paddingVertical: 4,
+  },
+  buttonStyle: {
+    borderRadius: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 18,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 30,
+  },
+  buttonText: {
+    color: "#eee",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 20,
+    
+  },
+  wrapperText: {
+    marginLeft: 10,
+    color: "#7d7d7d",
+    
+  },
+});
 
 export default Contact;
-
-const styles = StyleSheet.create({})
